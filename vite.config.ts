bb731a85e61293529d9168d931de9dd365c6f104
Brian Caldera <briancaldera07@gmail.com>
2025-01-12 02:27:36 -0400

    ⚛️ Add Vite React plugin
    -Setup lib mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import {defineConfig} from 'vite'
 import {resolve} from 'node:path'
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    plugins: [
+      react()
+    ],
     build: {
         lib: {
-            entry: resolve(__dirname, 'lib/main.ts'),
+            entry: resolve(__dirname, 'lib/main.js'),
             name: 'DentalPlaqueChart',
-            fileName: 'dental-plaque-chart',
+            fileName: format => `dental-plaque-chart.${format}.ts`,
         },
         rollupOptions: {
             external: ['react', 'react-dom'],
