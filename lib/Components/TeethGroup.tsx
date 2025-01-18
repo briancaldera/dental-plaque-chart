f13c3cfe3227151ae4fcc0db95ff71ef8d832b0b
Brian Caldera <briancaldera07@gmail.com>
2025-01-17 21:55:30 -0400

    ☕ Add disabled state
    -Add padding to Tooth.tsx
    -Add style for disable state

--- lib/Components/TeethGroup.tsx
+++ lib/Components/TeethGroup.tsx
@@ -7,16 +7,17 @@ type TeethGroupProps = {
     teeth: DentalPiece[]
     onClickSurface?: (id: string, surface: Surface, mark: boolean) => void
     onDiscardPiece?: (id: string) => void
+    disabled?: boolean
 }
 
-const TeethGroup = ({teeth, onClickSurface = () => {}, onDiscardPiece = () => {}}: TeethGroupProps) => {
+const TeethGroup = ({teeth, onClickSurface = () => {}, onDiscardPiece = () => {}, disabled = false}: TeethGroupProps) => {
 
     return (
         <div className='flex border'>
             {
                 teeth.map(tooth => <Tooth key={tooth.id} pieceModel={tooth} onClickSurface={(surface, mark) => {
                     onClickSurface(tooth.id, surface, mark)
-                }} onDiscardPiece={onDiscardPiece}/>)
+                }} onDiscardPiece={onDiscardPiece} disabled={disabled}/>)
             }
         </div>
     )
