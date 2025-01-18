34d8c65936a9318615e06f32618fdf82f93ac910
Brian Caldera <briancaldera07@gmail.com>
2025-01-18 15:09:20 -0400

    📖 Update README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
-import DentalPlaqueChart from "../lib/Components/DentalPlaqueChart.tsx";
-import {useDentalPlaqueChart} from "../lib/API/DentalPlaqueChartModel.ts";
+import DentalPlaqueChart from "../lib/components/DentalPlaqueChart.tsx";
+import {useDentalPlaqueChart} from "../lib/hooks/DentalPlaqueChartModel.ts";
 
 function App() {
 
     const dentalPlaqueChart = useDentalPlaqueChart()
+    dentalPlaqueChart.getPresentDentalPieces()
 
     return (
-        <div className='flex justify-center w-[800px]'>
+        <div className='flex justify-center items-center w-full p-10  h-[900px]'>
             <DentalPlaqueChart chart={dentalPlaqueChart}/>
         </div>
     )
